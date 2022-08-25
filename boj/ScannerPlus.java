b16a011bc54966c4799c5db804638bc846a405e8
stopHyun96 <stophyun96@gmail.com>
2022-08-26 02:35:54 +0900

    boj_exam

---
+++ boj/ScannerPlus.java
@@ -0,0 +1,19 @@
+package boj;
+
+import java.util.Scanner;
+
+public class ScannerPlus {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println("Case #" + i + ": " + (a + b));
+		}
+		
+	}
+}
