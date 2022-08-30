97f2c1d9324950ea66ebdfe8b7d732794f4a750e
stopHyun96 <stophyun96@gmail.com>
2022-08-30 23:50:08 +0900

    boj

---
+++ boj/APlusB2.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class APlusB2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String str=" ";
+		
+		while((str=br.readLine())!=null) {
+			st = new StringTokenizer(str);
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append(a + b).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
